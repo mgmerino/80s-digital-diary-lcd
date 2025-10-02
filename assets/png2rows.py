4ef90233da4e395d82a85b2c7f6cf78e41b41655
Manuel González <mgmerino@gmail.com>
2025-10-02 23:30:31 +0200

    Allow custom dimensions

--- assets/png2rows.py
+++ assets/png2rows.py
@@ -2,20 +2,24 @@
 from PIL import Image
 import sys
 
+WIDTH = int(sys.argv[2])
+HEIGHT = int(sys.argv[3])
+
+
 path = sys.argv[1]
-im = Image.open(path).convert("RGBA").resize((16, 16), Image.NEAREST)
+im = Image.open(path).convert("RGBA").resize((WIDTH, HEIGHT), Image.NEAREST)
 
 L = im.convert("L")
 A = im.getchannel("A")
 rows = []
-for y in range(16):
+for y in range(HEIGHT):
     val = 0
-    for x in range(16):
-        on = (A.getpixel((x,y)) > 0) and (L.getpixel((x,y)) < 64)
+    for x in range(WIDTH):
+        on = (A.getpixel((x,y)) > 0) and (L.getpixel((x,y)) < WIDTH//2)
         val = (val << 1) | (1 if on else 0)
     rows.append(val)
 
 print("ICON = [")
 for r in rows:
-    print(f"  0b{r:016b},")
+    print(f"  0b{r:0{WIDTH}b},")
 print("]")
