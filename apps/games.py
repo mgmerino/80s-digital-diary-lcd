c94aa2643c9740850de110f49c03c48b9bdba962
Manuel González <mgmerino@gmail.com>
2025-10-02 18:22:49 +0200

    Create games icon

--- apps/games.py
+++ apps/games.py
@@ -10,8 +10,31 @@ class GamesApp(App):
     tick_ms = 300
     
     def draw_icon(self, ctx, x, y, w, h):
-        # TODO: Copiar icon del main.py original
-        pass
+        icon = [
+            0b0000000000100000,
+            0b0000000001000000,
+            0b0000000010000000,
+            0b0000000100000000,
+            0b0000000100000000,
+            0b0111111111111110,
+            0b1000000000000001,
+            0b1001000000001001,
+            0b1011101010010001,
+            0b1001000000100001,
+            0b1000000000000001,
+            0b0111111111111110,
+            0b0000000000000000,
+            0b0000000000000000,
+            0b0000000000000000,
+            0b0000000000000000,
+        ]
+
+        start_x = x + (w - 16) // 2
+        start_y = y + (h - 12) // 2
+        for row, bits in enumerate(icon):
+            for col in range(16):
+                if bits & (1 << (15 - col)):
+                    ctx.d.pixel(start_x + col, start_y + row)
     
     def draw(self, ctx):
         cls(ctx)
